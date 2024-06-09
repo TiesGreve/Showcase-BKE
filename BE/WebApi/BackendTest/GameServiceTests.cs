fc2f58ec867b12233bef42515915337675a4b77c
TiesGreve <113978745+TiesGreve@users.noreply.github.com>
2024-06-09 09:41:11 +0200

    Eerste geschreven testen

--- BE/WebApi/BackendTest/GameServiceTests.cs
+++ BE/WebApi/BackendTest/GameServiceTests.cs
@@ -1,23 +1,104 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using WebApi.Controllers;
 using WebApi.Data;
+using WebApi.Interfaces.Services;
+using WebApi.Models;
+using NUnit.Framework;
 
 namespace BackendTest;
 
 public class Tests
 {
-    
-    
+
+    private Mock<DataContext> _dataContext;
+    private GameService _gameService;
     
     [SetUp]
     public void Setup()
     {
-        
+        _dataContext = new Mock<DataContext>(new DbContextOptions<DataContext>() );
+        _gameService = new GameService(_dataContext.Object);
     }
 
     [TestCase]
-    public void Test1()
+    public void Gamestart_NoParameters_SuccesfullStart()
+    {
+        var game = _gameService.SetGameStart(new Game
+        {
+            User1 = Guid.NewGuid(),
+            User2 = Guid.NewGuid()
+        });
+        
+        Assert.IsNotNull(game.CurrentTurn);
+        Assert.IsNotNull(game.FirstMove);
+        Assert.That(actual: game.FirstMove, Is.EqualTo(game.CurrentTurn));
+    }
+    
+    [TestCase(new string[]{ "x", "o", null, "x", "o", "o", "x", null, null }, GameState.Finnished)]
+    [TestCase(new string[]{ "x", "x", "x", null, "o", "o", "o", null, null }, GameState.Finnished)]
+    [TestCase(new string[]{ "x", "x", "x", null, "x", "o", "o", null, null }, GameState.Finnished)]
+    [TestCase(new string[]{ "x", "o", null, null, "x", "o", "o", null, "x" }, GameState.Finnished)]
+    [TestCase(new string[]{ "o", "x", "x", "o", "x", "o", "x", null, null }, GameState.Finnished)]
+    [TestCase(new string[]{ "x", "x", "x", "x", "x", "x", "x", "x", "x" }, GameState.Finnished)]
+    [TestCase(new string[]{ "o", "o", "o", "o", "o", "o", "o", "o", "o" }, GameState.Finnished)]
+    [TestCase(new string[]{ null, null, null, null, null, null, null, null, null}, GameState.Starting)]
+    [TestCase(new string[]{ "x", "x", "o", "o", "o", "x", "x", "x", "o" }, GameState.Draw)]
+    public void CheckGame_DifferentBoardConfigurations_GameResultChanges(string[] board, GameState result)
     {
         
+        var game = _gameService.CheckGame(new Game()
+        {
+            BoardState = board
+        });
+        Console.Write(game.GameState);
+        Assert.That(actual: game.GameState, Is.EqualTo(result));
+    }
+
+    [TestCase]
+    public void CheckIfStalemate()
+    {
+        
+    }
+    
+    [TestCase]
+    public void CheckVertical()
+    {
+        
+    }
+    [TestCase]
+    public void CheckHorizontal()
+    {
+        
+    }
+    [TestCase]
+    public void CheckDiagnals()
+    {
+        
+    }
+    [TestCase(1)]
+    public async Task MakeMove(int cell)
+    {
+        var currentTurn = Guid.NewGuid();
+        var gameId = Guid.NewGuid();
+        var game = new Game
+        {
+            Id = gameId,
+            User1 = currentTurn,
+            User2 = Guid.NewGuid(),
+            BoardState = new string[9],
+            CurrentTurn = currentTurn
+        };
+        var playingModel = new PlayingModel { GameID = gameId, Cell = 0 };
+        _dataContext.Setup(dc => dc.Games.Find(It.IsAny<int>())).Returns(game);
+        //_dataContext.Setup(dc => dc.SaveChangesAsync()).Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _gameService.MakeMove(playingModel);
+
+        // Assert
+        Assert.IsInstanceOf<OkObjectResult>(result);
     }
 }
--- BE/WebApi/WebApi/Services/GameService.cs
+++ BE/WebApi/WebApi/Services/GameService.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using WebApi.Data;
+using WebApi.Interfaces.Services;
 using WebApi.Models;
 
 namespace WebApi.Controllers;
 
-public class GameService
+public class GameService : IGameService
 {
-    private DataContext _dataContext;
+    public DataContext _dataContext { get; set; }
 
     public GameService(DataContext dataContext)
     {
@@ -31,79 +33,83 @@ public class GameService
             game.GameStart = DateTime.Now;
             return game;
         }
-        public Game CheckGame(Game game)
-        {
-            if (CheckVertical(game.BoardState) || CheckHorizontal(game.BoardState) || CheckDiagnals(game.BoardState)) { 
-                game.GameState = GameState.Finnished; 
-                game.GameFinish = DateTime.Now;
-                game.Winner = game.CurrentTurn;
-            }
-            else if (CheckIfStalemate(game))
-            {
-                game.GameState = GameState.Draw;
-                game.GameFinish = DateTime.Now;
-            }
-            return game;
+    public Game CheckGame(Game game)
+    {
+        if (CheckVertical(game.BoardState) || CheckHorizontal(game.BoardState) || CheckDiagnals(game.BoardState)) { 
+            game.GameState = GameState.Finnished; 
+            game.GameFinish = DateTime.Now;
+            game.Winner = game.CurrentTurn;
         }
-        private bool CheckIfStalemate(Game game)
+        else if (CheckIfStalemate(game))
         {
-            foreach(var item in game.BoardState)
-            {
-                if(item == null) return false;
-            }
-            return true;
+            game.GameState = GameState.Draw;
+            game.GameFinish = DateTime.Now;
         }
-        private bool CheckVertical(string[] board)
+        return game;
+    }
+    public bool CheckIfStalemate(Game game)
+    {
+        foreach(var item in game.BoardState)
         {
-            for(int i = 0; i < board.Length; i += 3)
-            {
-                if (board[0+i] != null && board[0+i] == board[1+i] && board[1+i] == board[2+i]) return true;
-            }
-            return false;
+            if(item == null || item.Equals(string.Empty)) return false;
         }
-        private bool CheckHorizontal(string[] board)
+        return true;
+    }
+    public bool CheckVertical(string[] board)
+    {
+        for(int i = 0; i < board.Length; i += 3)
         {
-            for (int i = 0; i < Math.Sqrt(board.Length); i ++)
-            {
-                if (board[i] != null && board[i] == board[3 + i] && board[3 + i] == board[6 + i]) return true;
-            }
-            return false;
+            if (board[0+i] == null || board[0+i].IsNullOrEmpty()) return false;
+            if (board[0+i] != null && board[0+i] == board[1+i] && board[1+i] == board[2+i]) return true;
         }
-        private bool CheckDiagnals(string[] board)
+        return false;
+    }
+    public bool CheckHorizontal(string[] board)
+    {
+        for (int i = 0; i < Math.Sqrt(board.Length); i ++)
         {
-            if (board[0] != null && board[0] == board[4] && board[4] == board[8]) return true;
-            if (board[2] != null && board[2] == board[4] && board[4] == board[6]) return true;
-            return false;
+            if (board[i] == null || board[i].IsNullOrEmpty()) return false;
+            if (board[i] != null && board[i] == board[3 + i] && board[3 + i] == board[6 + i]) return true;
         }
+        return false;
+    }
+    public bool CheckDiagnals(string[] board)
+    {
+        if (board[0] != null && board[0] == board[4] && board[4] == board[8]) return true;
+        if (board[2] != null && board[2] == board[4] && board[4] == board[6]) return true;
+        return false;
+    }
+    public async Task<IActionResult> MakeMove(PlayingModel playing)
+    {
+        Game? gameDb = _dataContext.Games.Find(playing.GameID);
+        if (playing.Cell is > 8 or < 0) return RequestService.ReturnBadRequest(nameof(MakeMove), "Illegal move");
+        if (gameDb == null) return RequestService.ReturnBadRequest(nameof(MakeMove), "Game not found");
+        if (gameDb.User2 == null) return RequestService.ReturnBadRequest(nameof(MakeMove), "No second user in game");
+        if (gameDb.BoardState[playing.Cell] != null) return RequestService.ReturnBadRequest(nameof(MakeMove), "Cell already filled in");
+        if (gameDb.CurrentTurn == gameDb.User1) gameDb.BoardState[playing.Cell] = "x";
+        else gameDb.BoardState[playing.Cell] = "o";
 
-        public async Task<IActionResult> MakeMove(PlayingModel playing)
+        gameDb = CheckGame(gameDb);
+        if (gameDb.GameFinish != null) HandleMove(gameDb);
+        
+        return new OkObjectResult(gameDb.GameState);
+    }
+
+    protected async void HandleMove(Game gameDb)
+    {
+        if (gameDb.CurrentTurn == gameDb.User1)
         {
-            Game? gameDb = _dataContext.Games.Find(playing.GameID);
-            if (playing.Cell > 8) return RequestService.ReturnBadRequest(nameof(MakeMove), "Illegal move");
-            if (gameDb == null) return RequestService.ReturnBadRequest(nameof(MakeMove), "Game not found");
-            if (gameDb.User2 == null) return RequestService.ReturnBadRequest(nameof(MakeMove), "No second user in game");
-            if (gameDb.BoardState[playing.Cell] != null) return RequestService.ReturnBadRequest(nameof(MakeMove), "Cell already filled in");
-            if (gameDb.CurrentTurn == gameDb.User1) gameDb.BoardState[playing.Cell] = "x";
-            else gameDb.BoardState[playing.Cell] = "o";
-
-            gameDb = CheckGame(gameDb);
-
-            if (gameDb.GameFinish == null)
-            {
-                if (gameDb.CurrentTurn == gameDb.User1)
-                {
-                    gameDb.CurrentTurn = gameDb.User2;
-                    gameDb.GameState = GameState.Player2Move;
-                }
-                else
-                {
-                    gameDb.CurrentTurn = gameDb.User1;
-                    gameDb.GameState = GameState.Player1Move;
-                }
-            }
-            gameDb.GameUpdate = DateTime.Now;
-            await _dataContext.SaveChangesAsync();
-            return new OkResult();
+            gameDb.CurrentTurn = gameDb.User2;
+            gameDb.GameState = GameState.Player2Move;
         }
+        else
+        {
+            gameDb.CurrentTurn = gameDb.User1;
+            gameDb.GameState = GameState.Player1Move;
+        }
+        
+        gameDb.GameUpdate = DateTime.Now;
+        await _dataContext.SaveChangesAsync();
+    }
         
 }
