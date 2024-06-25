21cac9daa9fa724634dfba8b2d9cd31bdf2789fa
TiesGreve <tiesgreve@gmail.com>
2024-06-25 11:30:09 +0200

    remove mvc

--- BE/ShowCase/ShowCase/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ShowCase.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
