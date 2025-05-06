0b9723d5ec654b1d924f5bcf6210ecdf6156d395
TiesGreve <113978745+TiesGreve@users.noreply.github.com>
2025-05-06 11:40:06 +0200

    login + register page

--- FE/frontend/src/app/login/page.tsx
+++ FE/frontend/src/app/login/page.tsx
@@ -1,6 +1,19 @@
+import Body from "@/components/body/body";
+
 export default function Page() {
-  
+
     return (
-        <div>login</div>
+        <div className="w-full flex flex-col justify-center items-center">
+              <h1>Login</h1>
+              <Body>
+                <form className="flex flex-col">
+                    <label>Email</label>
+                    <input type="email"></input>
+                    <label>Password</label>
+                    <input type="password"></input>
+                    <button className="button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> login</button>
+                </form>
+              </Body>
+            </div>
     )
 }
--- FE/frontend/src/app/page.tsx
+++ FE/frontend/src/app/page.tsx
@@ -1,5 +1,4 @@
 import Body from "@/components/body/body";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
@@ -8,9 +7,9 @@ export default function Page() {
     <div className="w-full flex flex-col justify-center items-center">
       <h1>TicTacToe</h1>
       <Body>
-      <Link href="/play" className="button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Play</Link>
-      <Link href="/login" className="button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</Link>
-      <Link href="/register" className="button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</Link>
+      <Link href="/play" className="">Play</Link>
+      <Link href="/login" className="">Login</Link>
+      <Link href="/register" className="">Register</Link>
       </Body>
     </div>
     
